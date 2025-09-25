d5e1c01962696cb1afb24abca119f78928ec8a3c
david-irvinne <davidneilleen@gmail.com>
2025-09-26 00:28:47 +0700

    feat: create basic user auth

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,13 @@ import express from "express";
 import "dotenv/config";
 
 import { connectDB } from "./db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({success: true, message: "test dulu ganteng"})
